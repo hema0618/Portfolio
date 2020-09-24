c5188bca5af220b5ad4c4ad0419308251716d298
Hema <khemalatha507@gmail.com>
2020-09-24 13:14:33 -0400

    link the pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from "react-mdl";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className="demo-big-content">
-    <Layout>
+      <Layout>
         <Header title="Title" scroll>
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
+          <Navigation>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/resume">Resume</Link>
+          </Navigation>
         </Header>
         <Drawer title="Title">
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
+          <Navigation>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/resume">Resume</Link>
+          </Navigation>
         </Drawer>
         <Content>
-            <div className="page-content" />
-            <Main/>
+          <div className="page-content" />
+          <Main />
         </Content>
-    </Layout>
-</div>
+      </Layout>
+    </div>
   );
 }
 
