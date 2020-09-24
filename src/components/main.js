12c70372a074884de5d8e27342649955877c1d6b
Hema <khemalatha507@gmail.com>
2020-09-24 13:10:32 -0400

    link the pages in main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Switch,Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import LandingPage from "./landingpage";
 import AboutMe from "./aboutme";
 import Contact from "./contact";
-import projects from "./projects";
-import Resume from  "./resume";
+import Projects from "./projects";
+import Resume from "./resume";
 
 const Main = () => (
 
     <switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
 
     </switch>
 )
-export default  Main;
+export default Main;
