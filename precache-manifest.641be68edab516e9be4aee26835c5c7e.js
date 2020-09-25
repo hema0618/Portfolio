9385622b179b02ec26b0cd5a07e7e806d97892dc
Hema <khemalatha507@gmail.com>
2020-09-25 15:27:22 -0400

    Updates

---
+++ precache-manifest.641be68edab516e9be4aee26835c5c7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe213e11c666a208c61febcdfd122dd0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8575ec7abfbe5c2d0a9a",
+    "url": "/Portfolio/static/css/2.22fc716b.chunk.css"
+  },
+  {
+    "revision": "6be7372935ed04468d09",
+    "url": "/Portfolio/static/css/main.46a6a43d.chunk.css"
+  },
+  {
+    "revision": "8575ec7abfbe5c2d0a9a",
+    "url": "/Portfolio/static/js/2.8888f8b2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.8888f8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be7372935ed04468d09",
+    "url": "/Portfolio/static/js/main.ffcc55d9.chunk.js"
+  },
+  {
+    "revision": "61b5e62bcd88dabf3a3a",
+    "url": "/Portfolio/static/js/runtime-main.07acc36a.js"
+  }
+]);
